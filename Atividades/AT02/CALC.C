06b91a57081fcb887e56ce335ba0cfef4e5d3b18
ITALO COSTA <costa.italo.ti@gmail.com>
2021-03-13 15:00:35 -0300

    arquivo hello world AT01 e calculadora AT02

---
+++ Atividades/AT02/CALC.C
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+	int main(){
+		int lista;
+		float num1,num2;
+		double result;	
+			
+			printf("opções:\n0-Sair\n1-somar\n2-Subtrair\n3-Mutiplicar\n4-Dividir\nDigite a função desejada\n");
+			scanf("%i",&lista);
+			
+				if ((lista>0)&&(lista<5)) {
+		
+					printf("\nDigite o primeiro valor a ser operado:\n");
+					scanf("%f",&num1);
+					printf("\nDigite o segundo valor a ser operado:\n");
+					scanf("%f",&num2);
+				}
+		
+		switch (lista){
+				
+				case 0:
+				return 0;
+				break;
+				
+				case 1 : result = num1+num2;
+				printf("\nO resultado eh=%f",result);
+				break;
+		
+				case 2: result = num1-num2;
+				printf("\nO resultado eh=%f",result);
+				break;
+				
+				case 3: result = num1*num2;
+				printf("\nO resultado eh=%f",result);
+				break;
+				
+				case 4: 
+					if(num2==0) 
+				printf("Não é possivel dividir por 0");
+					
+					else{	
+				result = num1/num2;
+				printf("\nO resultado eh=%f",result);
+				break;
+				}
+				
+				default: 
+				printf("Não é uma opção valida,encerrando programa");
+				return 0;
+				break;
+			
+	}		
+	}	
+	
