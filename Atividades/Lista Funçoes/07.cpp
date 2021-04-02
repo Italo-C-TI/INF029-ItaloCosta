1c8dbe643d9ee2ee0336a37b4a5ecf6273f2b368
ITALO COSTA <costa.italo.ti@gmail.com>
2021-04-01 21:48:42 -0300

    Retorna 3 Palavras

---
+++ Atividades/Lista Funçoes/07.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+typedef struct  {
+    char palavra1[128];
+    char palavra2[128];
+    char palavra3[128];
+} Palavras;
+
+Palavras ler3Palavras() {
+	 Palavras variavel;
+
+    printf("Digite a primeira palavra:\n");
+    fgets(variavel.palavra1, 128, stdin);
+
+    printf("Digite a segunda palavra:\n");
+    fgets(variavel.palavra2, 128, stdin);
+
+    printf("Digite a terceira palavra:\n");
+    fgets(variavel.palavra3, 128, stdin);
+
+    return variavel;
+}
+
+int main(){
+		Palavras palavras;
+    	palavras = ler3Palavras();
+
+    printf(" %s\n %s\n %s", palavras.palavra1, palavras.palavra2, palavras.palavra3);
+return 0;
+}
+
+
